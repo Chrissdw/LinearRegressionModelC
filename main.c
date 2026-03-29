58f128c3b5b866dbe66af228530176eab3e84e51
Chrissdw <cwardana6@gmail.com>
2026-03-29 23:43:30 +1100

    first commit

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct
+{
+    float independent_variables, dependent_variables[];
+
+} data;
+
+void train();
+void measure_error();
+void gradient_measure();
+void normalize();
+void json_to_array();
+
+void train()
+{
+}
+
+void measure_error()
+{
+}
+
+void main()
+{
+    printf("hello");
+}
