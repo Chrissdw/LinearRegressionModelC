28235729ecfd0ed7bfe26b5496e08c7be4df8c66
Chrissdw <cwardana6@gmail.com>
2026-03-30 00:27:36 +1100

    Fixed up some functions, worked on program flow a bit, continued to figure out functionality.

--- main.c
+++ main.c
@@ -6,13 +6,38 @@ typedef struct
 
 } data;
 
-void train();
+typedef struct
+{
+    float w, b;
+} linear_model;
+
+float model(linear_model *w, float iv, linear_model *b);
+void train(float iv[], float dv[]);
 void measure_error();
 void gradient_measure();
 void normalize();
 void json_to_array();
+void adjust_w();
+void adjust_b();
 
-void train()
+float model(linear_model *w, float iv, linear_model *b)
+{
+}
+
+void train(float iv[], float dv[])
+{
+    linear_model *w = 1;
+    linear_model *b = 1;
+    float ive = 10.4;
+    model(w, ive, b); // This will output the predicted dependent variable, after that just compare
+    measure_error();
+    gradient_measure();
+
+    adjust_w();
+    adjust_b();
+}
+
+void normalize()
 {
 }
 
@@ -20,7 +45,37 @@ void measure_error()
 {
 }
 
+void gradient_measure()
+{
+}
+
+void json_to_array()
+{
+}
+
+void adjust_w()
+{
+}
+void adjust_b()
+{
+}
+
 void main()
 {
-    printf("hello");
+    // Maybe use scanner to get input json file, then call json to array.
+
+    char json_file_x[] = "Will be scan input later";
+    char json_file_y[] = "Will be scan input later";
+
+    printf("Input Independent Variables:\n");
+    fgets(json_file_x, 0, stdin);
+
+    printf("Input Dependent Variables:\n");
+    fgets(json_file_y, 0, stdin);
+
+    float x[1];
+    float y[1];
+
+    json_to_array();
+    train(x, y);
 }
